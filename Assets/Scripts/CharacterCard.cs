b1211b99617daeda56f1ff8277950c2d741ed0e8
MagusConjurer <cameronisadavis@gmail.com>
2022-07-06 17:32:01 -0600

    Add placeholder sprite for character card. Begin setup for updating character stats in the CharacterCard script

--- Assets/Scripts/CharacterCard.cs
+++ Assets/Scripts/CharacterCard.cs
@@ -4,7 +4,13 @@ using UnityEngine;
 
 public class CharacterCard : Card
 {
+    [SerializeField] private int healthValue;
+    [SerializeField] private int strengthValue;
+    [SerializeField] private int accuracyValue;
+    [SerializeField] private int stealthValue;
+
     public int placedIndex;
+    
 
     private void OnMouseDown()
     {
@@ -19,5 +25,40 @@ public class CharacterCard : Card
     public void ResetColor()
     {
         cardSprite.color = cardSprite.color * 2;
+    }
+
+    public void SetHealth(int value)
+    {
+        SpriteRenderer[] healthSprites = GetStatSprites("Health");
+        SetStatSprites(healthSprites, value);
+    }
+
+    public void SetStrength(int value)
+    {
+        SpriteRenderer[] strengthSprites = GetStatSprites("Strength");
+        SetStatSprites(strengthSprites, value);
+    }
+
+    public void SetAccuracy(int value)
+    {
+        SpriteRenderer[] accuracySprites = GetStatSprites("Accuracy");
+        SetStatSprites(accuracySprites, value);
+    }
+
+    public void SetStealth(int value)
+    {
+        SpriteRenderer[] stealthSprites = GetStatSprites("Stealth");
+        SetStatSprites(stealthSprites, value);
+    }
+
+    private SpriteRenderer[] GetStatSprites(string type)
+    {
+        // TODO: Implement getting the sprite squares
+        return new SpriteRenderer[1];
+    }
+
+    private void SetStatSprites(SpriteRenderer[] sprites, int value)
+    {
+        // TODO: Implement setting the sprites
     }
 }
